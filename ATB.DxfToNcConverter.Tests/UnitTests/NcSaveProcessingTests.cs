d2ff0100965271bd675d1f365fab4c277ecb1c7f
picolino <webmaster.pico@gmail.com>
2020-07-02 11:25:27 +0300

    Add NcSaveProcessing tests

--- ATB.DxfToNcConverter.Tests/UnitTests/NcSaveProcessingTests.cs
+++ ATB.DxfToNcConverter.Tests/UnitTests/NcSaveProcessingTests.cs
@@ -1,8 +1,39 @@
-﻿using ATB.DxfToNcConverter.Systems;
+﻿using ATB.DxfToNcConverter.Components;
+using ATB.DxfToNcConverter.Systems;
+using Leopotam.Ecs;
+using NUnit.Framework;
 
 namespace ATB.DxfToNcConverter.Tests.UnitTests
 {
     public class NcSaveProcessingTests : SystemTestBase<NcSaveProcessing>
     {
+        protected override void BeforeInitializeSystems(EcsSystems systems)
+        {
+            ConfigurationServiceStub.WorkingDirectory = "C:\\tmp\\";
+        }
+
+        [Test]
+        public void SavedFileNameEqualToSourceDxfFileNameAndExtensionIsNc()
+        {
+            var entity = World.NewEntity();
+            entity.Get<DxfFileDefinition>().path = "C:\\tmp\\dxf_file.dxf";
+            entity.Get<NcProgram>().programText = "NCPROGRAMTEXT";
+            
+            System.Run();
+            
+            Assert.DoesNotThrow(() => { var _ = FileSystemServiceStub.SavedFiles["C:\\tmp\\dxf_file.nc"];});
+        }
+
+        [Test]
+        public void SavedFileConsistsOfNcProgramText()
+        {
+            var entity = World.NewEntity();
+            entity.Get<DxfFileDefinition>().path = "C:\\tmp\\dxf_file.dxf";
+            entity.Get<NcProgram>().programText = "NCPROGRAMTEXT";
+            
+            System.Run();
+            
+            Assert.That(FileSystemServiceStub.SavedFiles["C:\\tmp\\dxf_file.nc"], Is.EqualTo("NCPROGRAMTEXT"));
+        }
     }
 }
